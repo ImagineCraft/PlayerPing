60f04f04f5a688f9452daa677e4baeaccc965976
sirrus86 <sirrus86@hotmail.com>
2016-09-05 22:56:17 -0400

    Revamp code to be more lightweight and efficient

--- PlayerPing/src/com/cyber/ping/PlayerPing.java
+++ PlayerPing/src/com/cyber/ping/PlayerPing.java
@@ -6,8 +6,8 @@
 package com.cyber.ping;
 
 import net.minecraft.server.v1_10_R1.EntityPlayer;
+
 import org.bukkit.ChatColor;
-import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.craftbukkit.v1_10_R1.entity.CraftPlayer;
@@ -18,77 +18,40 @@ import org.bukkit.plugin.java.JavaPlugin;
  *
  * @author robin
  */
-public class PlayerPing extends JavaPlugin{
-    
-    Server server;
-    
-    @Override
-    public void onEnable(){
-        System.out.println("PlayerPing enabled");
-        server = this.getServer();
-    }
-    
-    @Override
-    public void onDisable(){
-        System.out.println("PlayerPing disabled");
-    }
+public class PlayerPing extends JavaPlugin {
     
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.RED + "The console cannot perform this command!");
-            return true;
+        Player target = null;
+        if (args.length > 0) {
+            target = getPlayer(args[0]);
+            if (target == null) {
+                sender.sendMessage(ChatColor.RED + "Can't find player " + args[0]);
+                return true;
+            }
         }
-        if(command.getName().equalsIgnoreCase("ping")){
-            Player target;
-            if(args.length == 0){
-                target = (Player) sender;
-                printPing((Player)sender, target);
-            }
-            else if(isPlayer(args[0])){
-                target = getPlayer(args[0]);
-                printPing((Player)sender, target);
-            }
-            else{
-                sender.sendMessage(ChatColor.RED+"Can't find player "+args[0]);
-            }
+        else if (sender instanceof Player) target = (Player)sender;
+        if (target != null) {
+            int ping = getPing(target);
+            sender.sendMessage(ChatColor.GOLD + (target == sender ? "Your" : target.getDisplayName() + "'s") + " current ping: " + ChatColor.DARK_GRAY + "[" + ChatColor.DARK_AQUA + ping + "ms" + ChatColor.DARK_GRAY + "]");
+            return true;
         }
         return false;
     }
     
-    private Integer getPing(Player p){
+    private int getPing(Player p) {
         CraftPlayer pingc = (CraftPlayer) p;
         EntityPlayer pinge = pingc.getHandle();
         return pinge.ping;
     }
     
-    private boolean isPlayer(String name){
-        for(Player p : server.getOnlinePlayers()){
-            if(p.getName().equalsIgnoreCase(name)){
-                return true;
-            }
-        }
-        return false;
-    }
     
-    
-    private Player getPlayer(String name){
-        for(Player p : server.getOnlinePlayers()){
+    private Player getPlayer(String name) {
+        for(Player p : getServer().getOnlinePlayers()){
             if(p.getName().equalsIgnoreCase(name)){
                 return p;
             }
         }
         return null;
-    }
-    
-    private void printPing(Player sender, Player target){
-        Integer ping = Integer.valueOf(getPing(target));
-        
-        if(sender.equals(target)){
-            sender.sendMessage(ChatColor.GOLD+"Your current ping: " + ChatColor.DARK_GRAY + "[" + ChatColor.DARK_AQUA + ping + "ms" + ChatColor.DARK_GRAY + "]");
-        }
-        else{
-            sender.sendMessage(ChatColor.GOLD+target.getDisplayName()+"'s current ping: " + ChatColor.DARK_GRAY + "[" + ChatColor.DARK_AQUA + ping + "ms" + ChatColor.DARK_GRAY + "]");
-        }
     }
 }
